f8a248e52246148e2c597565f482cab58992517b
justinaray <justin.a.ray@gmail.com>
2017-02-15 10:52:21 -0500

    Add autofix example to source and presentation

--- examples/welcome-to-eslint.js
+++ examples/welcome-to-eslint.js
@@ -1,4 +1,9 @@
 /**
- * Sample file for showing some basic linting errors and reporting
+ * Sample file for showing some basic linting errors, reporting, and
+ * autofix
  */
 var foo = bar;
+
+function soMuchLint(choice) {
+  return choice?"tabs":"spaces"
+}
